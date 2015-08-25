c07565f7ce36df613e03aac9ffaaf492f2cc19a6
Stevie Parris <Stevie.Parris@gmail.com>
2015-08-25 01:49:04 -0400

    Added support for adding new blogs via the addUser page. It is only using the username field at the moment. Next steps are looking to database implementation, and using the data from that page, working into a json.

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var spp53 = require('spp53');
+// -- Please don't touch -- ghost require creation -- //
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -18,7 +20,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,12 +28,18 @@ app.use('/', routes);
 app.use('/users', users);
 
 
+spp53().then(function (ghostServer) {
+    app.use('/spp53', ghostServer.rootApp);
+    ghostServer.start(app);
+});
+// -- Please don't touch -- ghost route creation -- //
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
-});
+});*/
 
 // error handlers
 
@@ -56,5 +64,6 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,8 @@ var fs = require('fs');
 var http = require('http');
 var path = require('path');
 var uploadManager = require('./uploadManager')(router);
-
-//window.$ = window.jQuery = require('jquery');
-//global.jQuery = require('jquery');
-//var bootstrap = require('bootstrap');
+var ncp = require('ncp').ncp;
+var fs = require('fs');
 
 router.get('/addUser', function(req, res, next){
 /*	fs.readFile(req.files.displayImage.path, function (err, data) {
@@ -19,6 +17,18 @@ router.get('/addUser', function(req, res, next){
 	});
 });*/
 	res.render('addUser', { title: 'Pioneer Students' });
+});
+
+router.post('/createuser', function(req,res){
+
+	//if the file exists
+	if (createWebsite(req.body.username)) {
+		res.redirect('/addUser');
+	}
+	else {
+		res.render('error', { title: 'Username/website already exists.'});
+	}
+	//add res.body information to database, and write to the map json as well.
 });
 
 /* GET home page. */
@@ -92,5 +102,72 @@ router.get('/', function(req, res, next) {
 	*/
 	res.render('index', { title: 'Pioneer Students' });
 });
+
+function createWebsite(username){
+	console.log("Creating website for username: " + username);
+        source = "./node_modules/ghost";
+        destination = "./node_modules/" + username;
+
+	if(!fs.exists(destination))
+	{
+		//if the file exists
+		//copy file from source to destination
+		ncp(source, destination, function (err) {
+			if (err) {
+				return console.error(err);
+			}
+			//replace config.js with the example version
+			oldFile = fs.createReadStream(destination + "/config.example.js");
+			newFile = fs.createWriteStream(destination + "/config.js");
+			oldFile.pipe(newFile);
+			//modify config.js using the new username
+			fs.readFile(destination + '/config.js', 'utf8', function read(err, data) {
+				if (err) {
+					throw err;
+				}
+
+				//Change URLS and port numbers
+				oldurl = "my-ghost-blog.com"
+				newurl = "PioneerStudents.net"
+				portNumb1 = "2368";
+				portNumb2 = "2369";
+
+				//This will need to be stored in a database and incremented as we go
+				lastPortNumber = "2750";
+				newPortNumb1 = (parseInt(lastPortNumber) + 1).toString();
+				newPortNumb2 = (parseInt(lastPortNumber) + 2).toString();
+				data = data.replace(new RegExp(oldurl, 'g'),newurl);
+				data = data.replace(new RegExp(portNumb1, 'g'),newPortNumb1);
+				data = data.replace(new RegExp(portNumb2, 'g'),newPortNumb2);
+				data = data.replace(new RegExp('http://localhost:' + newPortNumb1, 'g'),'http://localhost:'+newPortNumb1+'/'+username);
+				fs.writeFile(destination + "/config.js", data, function(err) {
+					if(err) {
+						return console.log(err);
+					}
+				});
+			});
+
+			//Modifying app.js to add require and new ghostserver. Replaces comment in place.
+			fs.readFile('./app.js', 'utf8', function read(err, data) {
+				requireStr = "var " + username + " = require('" + username + "');";
+				requireSearchStr = "// -- Please don't touch -- ghost require creation -- //";
+				routeStr = username + "().then(function (ghostServer) { app.use('/" + username + "', ghostServer.rootApp);ghostServer.start(app);});";
+				routeSearchStr = "// -- Please don't touch -- ghost route creation -- //";
+				data = data.replace(new RegExp(requireSearchStr, 'g'),requireStr + '\n' + requireSearchStr);
+				data = data.replace(new RegExp(routeSearchStr, 'g'),routeStr + '\n' + routeSearchStr);
+				fs.writeFile("./app.js", data, function(err) {
+					if(err) {
+						return console.log(err);
+					}
+				});
+			});
+
+		});
+		return true;
+	} else {
+		return false;
+	}
+}
+
 
 module.exports = router;
